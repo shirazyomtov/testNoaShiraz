8f0f46eebaa1966f185f0b8afdcc661bbd79df5f
noaMalkaManzur <noamanzur1998@gmail.com>
2023-05-17 19:28:17 +0300

    after gal

--- ESP32CAM_Car/ESP32CAM_Car.ino
+++ ESP32CAM_Car/ESP32CAM_Car.ino
@@ -1,48 +1,33 @@
-
 /*
- * @Date: 2020-11-27 11:45:09
- * @Description: ESP32 Camera Surveillance Car
- * @FilePath: 
- */
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-cam-post-image-photo-server/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
 
-#include "esp_camera.h"
+#include <Arduino.h>
 #include <WiFi.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "esp_camera.h"
 
-//
-// WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
-//            or another board which has PSRAM enabled
-//
-// Adafruit ESP32 Feather
+const char* ssid = "AndroidAP";
+const char* password = "miuz4783";
 
-// Select camera model
-//#define CAMERA_MODEL_WROVER_KIT
-//#define CAMERA_MODEL_M5STACK_PSRAM
-#define CAMERA_MODEL_AI_THINKER
+String serverName = "192.168.43.121";   // REPLACE WITH YOUR Raspberry Pi IP ADDRESS
+//String serverName = "example.com";   // OR REPLACE WITH YOUR DOMAIN NAME
 
-const char* ssid = "shiraz";   //Enter SSID WIFI Name
-const char* password = "1822shyo";   //Enter WIFI Password
+String serverPath = "/upload";     // The default serverPath should be upload.php
 
-#if defined(CAMERA_MODEL_WROVER_KIT)
-#define PWDN_GPIO_NUM    -1
-#define RESET_GPIO_NUM   -1
-#define XCLK_GPIO_NUM    21
-#define SIOD_GPIO_NUM    26
-#define SIOC_GPIO_NUM    27
+const int serverPort = 80;
 
-#define Y9_GPIO_NUM      35
-#define Y8_GPIO_NUM      34
-#define Y7_GPIO_NUM      39
-#define Y6_GPIO_NUM      36
-#define Y5_GPIO_NUM      19
-#define Y4_GPIO_NUM      18
-#define Y3_GPIO_NUM       5
-#define Y2_GPIO_NUM       4
-#define VSYNC_GPIO_NUM   25
-#define HREF_GPIO_NUM    23
-#define PCLK_GPIO_NUM    22
+WiFiClient client;
 
-
-#elif defined(CAMERA_MODEL_AI_THINKER)
+// CAMERA_MODEL_AI_THINKER
 #define PWDN_GPIO_NUM     32
 #define RESET_GPIO_NUM    -1
 #define XCLK_GPIO_NUM      0
@@ -61,38 +46,25 @@ const char* password = "1822shyo";   //Enter WIFI Password
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
 
-#else
-#error "Camera model not selected"
-#endif
-
-// GPIO Setting
-extern int gpLb =  2; // Left 1
-extern int gpLf = 14; // Left 2
-extern int gpRb = 15; // Right 1
-extern int gpRf = 13; // Right 2
-extern int gpLed =  4; // Light
-extern String WiFiAddr ="";
-
-void startCameraServer();
+const int timerInterval = 30000;    // time between each HTTP POST image
+unsigned long previousMillis = 0;   // last time image was sent
 
 void setup() {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); 
   Serial.begin(115200);
-  Serial.setDebugOutput(true);
+
+  WiFi.mode(WIFI_STA);
   Serial.println();
-
-
-  pinMode(gpLb, OUTPUT); //Left Backward
-  pinMode(gpLf, OUTPUT); //Left Forward
-  pinMode(gpRb, OUTPUT); //Right Forward
-  pinMode(gpRf, OUTPUT); //Right Backward
-  pinMode(gpLed, OUTPUT); //Light
-
-  //initialize
-  digitalWrite(gpLb, LOW);
-  digitalWrite(gpLf, LOW);
-  digitalWrite(gpRb, LOW);
-  digitalWrite(gpRf, LOW);
-  digitalWrite(gpLed, LOW);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);  
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("ESP32-CAM IP Address: ");
+  Serial.println(WiFi.localIP());
 
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
@@ -115,53 +87,198 @@ void setup() {
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  //init with high specs to pre-allocate larger buffers
+
+  // init with high specs to pre-allocate larger buffers
   if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 10;  //0-63 lower number means higher quality
     config.fb_count = 2;
   } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;
+    config.frame_size = FRAMESIZE_CIF;
+    config.jpeg_quality = 12;  //0-63 lower number means higher quality
     config.fb_count = 1;
   }
-
+  
   // camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
-    return;
+    delay(1000);
+    ESP.restart();
   }
 
-  //drop down frame size for higher initial frame rate
-  sensor_t * s = esp_camera_sensor_get();
-  s->set_framesize(s, FRAMESIZE_CIF);
-
-#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM) || defined(CAMERA_MODEL_AI_THINKER)
-  s->set_vflip(s, 0);
-  s->set_hmirror(s, 0);
-#endif
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-
-  startCameraServer();
-
-  Serial.print("Camera Ready! Use 'http://");
-  Serial.print(WiFi.localIP());
-  WiFiAddr = WiFi.localIP().toString();
-  Serial.println("' to connect");
+  //establishConnection();
 }
 
-void loop() 
-{
-  
-// put your main code here, to run repeatedly:
+void loop() {
+ // unsigned long currentMillis = millis();
+ // if (currentMillis - previousMillis >= timerInterval) {
+  //while (true) {
+    sendPhoto();
+    //   previousMillis = currentMillis;
+    // }
+    Serial.println("Photo sent");
+    delay(100);
+ // }
+}
+void establishConnection() {
+  // Establish the initial connection with the server
+  if (client.connect(serverName.c_str(), serverPort)) {
+    Serial.println("Connection successful!");
+  } else {
+    Serial.println("Connection to " + serverName + " failed.");
+  }
+}
 
+void closeConnection() {
+  // Close the connection with the server
+  client.stop();
+  Serial.println("Connection closed.");
+}
+// void sendPhoto() {
+//   String getAll;
+//   String getBody;
+
+//   camera_fb_t *fb = NULL;
+//   fb = esp_camera_fb_get();
+//   if (!fb) {
+//     Serial.println("Camera capture failed");
+//     delay(1000);
+//     ESP.restart();
+//   }
+
+//   Serial.println("Sending photo to the server...");
+
+//   String head = "--RandomNerdTutorials\r\nContent-Disposition: form-data; name=\"imageFile\"; filename=\"esp32-cam.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
+//   String tail = "\r\n--RandomNerdTutorials--\r\n";
+
+//   uint32_t imageLen = fb->len;
+//   uint32_t extraLen = head.length() + tail.length();
+//   uint32_t totalLen = imageLen + extraLen;
+
+//   client.println("POST " + serverPath + " HTTP/1.1");
+//   client.println("Host: " + serverName);
+//   client.println("Content-Length: " + String(totalLen));
+//   client.println("Content-Type: multipart/form-data; boundary=RandomNerdTutorials");
+//   client.println();
+//   client.print(head);
+
+//   uint8_t *fbBuf = fb->buf;
+//   size_t fbLen = fb->len;
+//   for (size_t n = 0; n < fbLen; n = n + 1024) {
+//     if (n + 1024 < fbLen) {
+//       client.write(fbBuf, 1024);
+//       fbBuf += 1024;
+//     } else if (fbLen % 1024 > 0) {
+//       size_t remainder = fbLen % 1024;
+//       client.write(fbBuf, remainder);
+//     }
+//   }
+//   client.print(tail);
+
+//   esp_camera_fb_return(fb);
+
+//   int timoutTimer = 10000;
+//   long startTimer = millis();
+//   boolean state = false;
+
+//   while ((startTimer + timoutTimer) > millis()) {
+//     Serial.print(".");
+//     //delay(100);
+//     while (client.available()) {
+//       char c = client.read();
+//       if (c == '\n') {
+//         if (getAll.length() == 0) {
+//           state = true;
+//         }
+//         getAll = "";
+//       } else if (c != '\r') {
+//         getAll += String(c);
+//       }
+//       if (state == true) {
+//         getBody += String(c);
+//       }
+//       startTimer = millis();
+//     }
+//     if (getBody.length() > 0) {
+//       break;
+//     }
+//   }
+//   Serial.println();
+//   Serial.println(getBody);
+// }
+String sendPhoto() {
+  String getAll;
+  String getBody;
+
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();
+  if(!fb) {
+    Serial.println("Camera capture failed");
+    delay(1000);
+    ESP.restart();
+  }
+  
+  Serial.println("Connecting to server: " + serverName);
+
+  if (client.connect(serverName.c_str(), serverPort)) {
+    Serial.println("Connection successful!");    
+    String head = "--RandomNerdTutorials\r\nContent-Disposition: form-data; name=\"imageFile\"; filename=\"esp32-cam.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
+    String tail = "\r\n--RandomNerdTutorials--\r\n";
+
+    uint32_t imageLen = fb->len;
+    uint32_t extraLen = head.length() + tail.length();
+    uint32_t totalLen = imageLen + extraLen;
+  
+    client.println("POST " + serverPath + " HTTP/1.1");
+    client.println("Host: " + serverName);
+    client.println("Content-Length: " + String(totalLen));
+    client.println("Content-Type: multipart/form-data; boundary=RandomNerdTutorials");
+    client.println();
+    client.print(head);
+  
+    uint8_t *fbBuf = fb->buf;
+    size_t fbLen = fb->len;
+    for (size_t n=0; n<fbLen; n=n+1024) {
+      if (n+1024 < fbLen) {
+        client.write(fbBuf, 1024);
+        fbBuf += 1024;
+      }
+      else if (fbLen%1024>0) {
+        size_t remainder = fbLen%1024;
+        client.write(fbBuf, remainder);
+      }
+    }   
+    client.print(tail);
+    
+    esp_camera_fb_return(fb);
+    
+    int timoutTimer = 10000;
+    long startTimer = millis();
+    boolean state = false;
+    
+    while ((startTimer + timoutTimer) > millis()) {
+      Serial.print(".");
+      delay(100);      
+      while (client.available()) {
+        char c = client.read();
+        if (c == '\n') {
+          if (getAll.length()==0) { state=true; }
+          getAll = "";
+        }
+        else if (c != '\r') { getAll += String(c); }
+        if (state==true) { getBody += String(c); }
+        startTimer = millis();
+      }
+      if (getBody.length()>0) { break; }
+    }
+    Serial.println();
+    client.stop();
+    Serial.println(getBody);
+  }
+  else {
+    getBody = "Connection to " + serverName +  " failed.";
+    Serial.println(getBody);
+  }
+  return getBody;
 }
